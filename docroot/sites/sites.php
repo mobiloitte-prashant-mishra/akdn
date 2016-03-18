9b4c70445ef178d086aa6b21b1bdf282713dedb3
Ajay Mishra <ajay.mishra@srijan.in>
2016-03-18 11:02:04 +0530

    Added sites file

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -22,5 +22,6 @@ $sites = array(
   "akdndev.prod.acquia-sites.com" => 'akdn',
  "akdn3dev.prod.acquia-sites.com" => 'akdn',
  "akdn3stg.prod.acquia-sites.com" => 'akdn',
+ "akdnredesign.com" => "akdn",
 );
 ?>
