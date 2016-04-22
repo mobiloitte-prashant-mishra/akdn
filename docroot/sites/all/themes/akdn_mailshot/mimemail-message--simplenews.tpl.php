2c0d2310ea1ded8e39b4128686e8f4d97a1e7bec
shishir <shishircoolin@gmail.com>
2016-04-22 12:27:06 +0530

    akdn show single line in newsletter confirmation email

--- docroot/sites/all/themes/akdn_mailshot/mimemail-message--simplenews.tpl.php
+++ docroot/sites/all/themes/akdn_mailshot/mimemail-message--simplenews.tpl.php
@@ -37,15 +37,15 @@
           <div class="logo" style="text-align: center; border-top: 30px solid #b49759; padding-top: 10px; padding-bottom: 8px;">
             <a href = 'http://akdndev.prod.acquia-sites.com/' style="display: block; margin: auto;"><img <?php print 'src= http://srijanaravali.github.io/akdn/images/akdn-logo.png' ?> alt="<?php print t('Home'); ?>" style="width: 120px; height: 55px; margin:10px auto" /></a>
           </div>
+          <div class="mail_body"><?php print $body;?></div>
           <div class="header">
-            <p class="first"><span class="mail-shot-date" style="font-weight:bold;" ><?php print date('j F Y') . " -"; ?></span> The following content was published at <a href= <?php print $base_url ?>
+            <!--p class="first"><span class="mail-shot-date" style="font-weight:bold;" ><?php print date('j F Y') . " -"; ?></span> The following content was published at <a href= <?php print $base_url ?>
                >http://akdndev.prod.acquia-sites.com/</a>, the official website of the AGA Khan Development Network.
-            </p>
+            </p-->
             <div class="last">
              <i>Do you have friends and family members who would like to receive this newsletter? Invite them to sign up at</i> <a href="subscribe">http://akdndev.prod.acquia-sites.com/subscribe.</a>
             </div>
           </div>
-        <div class="mail_body"><?php print $body;?></div>
       </div>
     </div>
     <div class="band" style="text-align: center; border-top: 30px solid #b49759; margin-top: 10px; clear:both;"></div>
