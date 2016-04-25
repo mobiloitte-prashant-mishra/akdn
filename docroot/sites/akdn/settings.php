5c6eeb892b70eedfc114e4c81ed8ec1484c149f4
Akash Bhawra <akashbhawra@gmail.com>
2016-04-26 00:22:47 +0530

    committed

--- docroot/sites/akdn/settings.php
+++ docroot/sites/akdn/settings.php
@@ -231,7 +231,7 @@ $drupal_hash_salt = '';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
-$base_url = 'http://akdn3.prod.acquia-sites.com';  // NO trailing slash!
+$base_url = 'http://akdn3stg.gprod.acquia-sites.com';  // NO trailing slash!
 
 /**
  * PHP settings:
