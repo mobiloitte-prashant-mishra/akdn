deedd7bc8b992d0666d0ecb24f5685b26c9e62e6
shishir <shishircoolin@gmail.com>
2016-11-03 18:07:56 +0530

    AKDN Newsletter changes + Reduce steps + Remove custom social fields

---
+++ docroot/sites/akdn/modules/custom/scs_mailchimp_creation/scs_mailchimp_creation_links.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * theme implementation to display Links.
+ */
+
+?>
+<ul class="tabs primary">
+  <li><?php print $send; ?></li>
+  <li><?php print $send_test; ?></li>
+</ul>
