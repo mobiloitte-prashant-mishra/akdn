fe7624e5189905a4d5e27ba5d5ad51a3d64cd051
vermauv <vermaudit28@gmail.com>
2016-11-30 14:53:35 +0530

    AKDN-1431 : AKDN cleanup

--- docroot/sites/ecard/modules/contrib/fullcalendar/fullcalendar_options/js/fullcalendar_options.fullcalendar.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-
-Drupal.fullcalendar.plugins.fullcalendar_options = {
-  options: function (fullcalendar) {
-    return Drupal.settings.fullcalendar[fullcalendar.dom_id].fullcalendar_options;
-  }
-};
-
-}(jQuery));
--- docroot/sites/ismaili/modules/custom/ismaili_custom/js/home_page.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * A javascript file for the home page.
- */
-jQuery(document).ready(function() {
-  if( !jQuery.trim(jQuery('.panels-flexible-region-1-subscribe-inside').html()).length) {
-    jQuery('.panels-flexible-region-1-about_ismaili').css('width', '100%');
-  }
-});
