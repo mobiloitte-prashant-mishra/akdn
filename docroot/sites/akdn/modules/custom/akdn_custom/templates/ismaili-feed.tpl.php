9b0cfb676fc56a2709cc7061574469596a2843b5
Maninder Singh <maninders@srijan.in>
2017-08-04 17:58:56 +0530

    internal page news community theme

--- docroot/sites/akdn/modules/custom/akdn_custom/templates/ismaili-feed.tpl.php
+++ docroot/sites/akdn/modules/custom/akdn_custom/templates/ismaili-feed.tpl.php
@@ -1,4 +1,5 @@
-<div clas="">
+<div class="news-community">
+    <div class="view-content">
   <?php
   foreach ($output as $key => $value) {
 //    print '<pre>';
@@ -10,39 +11,46 @@
     $body = $value->Body;
     $read_more = $value->Read-More;
     ?>
+    
+        <div class="news-block-content">
     <?php if ($image) { ?>
-      <div class="">
+      <div class="news-image">
         <?php print $image; ?>
       </div>
     <?php } ?>
     <?php if ($primary_cat) { ?>
-      <div class="">
+      <div class="article-type">
         <?php print $primary_cat; ?>
       </div>
     <?php } ?>
     <?php if ($title) { ?>
-      <div class="">
+       <div class="">
+           <h4>
         <?php print $title; ?>
+          </h4>
       </div>
     <?php } ?>
     <?php if ($date) { ?>
-      <div class="">
+      <div class="news-date">
         <?php print $date; ?>
       </div>
     <?php } ?>
     <?php if ($body) { ?>
-      <div class="">
+      <div class="news-body">
         <?php print $body; ?>
       </div>
     <?php } ?>
   <?php if ($read_more) { ?>
-      <div class="">
+      <div class="readmore-text">
         <?php print $read_more; ?>
       </div>
     <?php } ?>
+            </div>
     <?php
 //  print '<pre>';
 //  print_r($value); die;
   }
   ?>
+    
+    </div>
 </div>
