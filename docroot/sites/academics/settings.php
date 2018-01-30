6f3b3fadce2aea6c2d48958b32c9831d99ad6bac
kkalashnikov <kunal.gautam26@gmail.com>
2018-01-30 10:26:48 +0530

    update academics settings.php

--- docroot/sites/academics/settings.php
+++ docroot/sites/academics/settings.php
@@ -185,7 +185,7 @@ $databases = array (
       'database' => 'akdn3db165449',
       'username' => 's21222',
       'password' => 'KTLM4nDPRMZ5PZR',
-      'host' => '127.0.0.1',
+      'host' => '34.250.132.39'
       'port' => '',
       'driver' => 'mysql',
       'prefix' => '',
@@ -245,7 +245,7 @@ $drupal_hash_salt = 'UY2huecCJOhaVGOxgJdQM4QWuXsPbLQiJMZU-SlfidU';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
- #$base_url = 'http://akdn3academies1.prod.acquia-sites.com';  // NO trailing slash!
+ $base_url = 'http://new.academies.org/';  // NO trailing slash!
 
 /**
  * PHP settings:
