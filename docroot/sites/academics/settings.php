73ad77644c8dbaefc8f460bfa0544b3de29d2247
kkalashnikov <kunal.gautam26@gmail.com>
2018-03-21 16:19:36 +0530

    change in settings.php

--- docroot/sites/academics/settings.php
+++ docroot/sites/academics/settings.php
@@ -182,9 +182,9 @@ $databases = array (
   array (
     'default' =>
     array (
-      'database' => 'akdn3academies2',
-      'username' => 's39874',
-      'password' => 'aFfBj8VFX2XPEL7',
+      'database' => 'akdn3academies1',
+      'username' => 's39873',
+      'password' => '9m3T72xRFhLNepf',
       'host' => '127.0.0.1',
       'port' => '',
       'driver' => 'mysql',
@@ -246,7 +246,7 @@ $drupal_hash_salt = 'UY2huecCJOhaVGOxgJdQM4QWuXsPbLQiJMZU-SlfidU';
  * for you.
  */
 
- $base_url = 'http://new.agakhanacademies.org';  // NO trailing slash
+# $base_url = 'http://new.agakhanacademies.org';  // NO trailing slash
 
 /**
  * PHP settings:
