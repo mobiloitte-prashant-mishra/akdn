2c7eb1ff42e8be9cdeb6ab3747db8891970b11f4
naresh bavaskar <naresh3419@gmail.com>
2018-07-13 11:29:21 +0530

    theme file updated

--- docroot/sites/akdn/themes/akdn/template.php
+++ docroot/sites/akdn/themes/akdn/template.php
@@ -394,6 +394,7 @@ function akdn_colorbox_imagefield($variables) {
 function akdn_preprocess_panels_pane(&$vars) {
   if (isset($vars['pane']->pid) && $vars['pane']->pid == "1495") {
     if (isset($vars['content']['#field_name'])) {
+      print("<pre>".print_r($vars['title'], true)."</pre>");exit;
       if ($vars['content']['#field_name'] == "field_project_links") {
         $vars['title'] = t('Related links');
       }
